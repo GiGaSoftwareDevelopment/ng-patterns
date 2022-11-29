4be980f03e94cfe9d720c4017aff1e6b781272b0
jerryorta.dev@gmail.com <jerryorta.dev@gmail.com>
2022-11-29 07:15:05 -0600

    feat(data): migrate

---
+++ libs/date/src/lib/millisecond.spec.ts
@@ -0,0 +1,54 @@
+/**
+ * @license
+ * Copyright UIUX Engineering All Rights Reserved.
+ */
+
+import { dayToMs, hourToMs, minToMs, msToDays, msToHours, msToMins, msToSec, secToMs } from './millisecond';
+
+describe('Time', () => {
+  beforeEach(() => {
+    // spyOn option
+  });
+
+  afterEach(() => {});
+
+  it('should convert milliseconds to seconds', () => {
+    const seconds: number = msToSec(5000);
+    expect(seconds).toEqual(5);
+  });
+
+  it('should convert sec to ms', () => {
+    const s: number = secToMs(5);
+    expect(s).toEqual(5000);
+  });
+
+  it('should convert milliseconds to minutes', () => {
+    const minutes: number = msToMins(120000);
+    expect(minutes).toEqual(2);
+  });
+
+  it('should convert min to ms', () => {
+    const m: number = minToMs(2);
+    expect(m).toEqual(120000);
+  });
+
+  it('should convert milliseconds to hours', () => {
+    const hours: number = msToHours(7200000);
+    expect(hours).toEqual(2);
+  });
+
+  it('should convert hour to ms', () => {
+    const h: number = hourToMs(2);
+    expect(h).toEqual(7200000);
+  });
+
+  it('should convert milliseconds to days', () => {
+    const days: number = msToDays(172800000);
+    expect(days).toEqual(2);
+  });
+
+  it('should convert day to ms', () => {
+    const d: number = dayToMs(2);
+    expect(d).toEqual(172800000);
+  });
+});
---
+++ libs/date/src/lib/millisecond.ts
@@ -0,0 +1,46 @@
+/**
+ * @license
+ * Copyright UIUX Engineering All Rights Reserved.
+ */
+
+/*
+x = ms / 1000
+seconds = x % 60
+x /= 60
+minutes = x % 60
+x /= 60
+hours = x % 24
+x /= 24
+days = x
+ */
+export function msToSec(_ms: number | string): number {
+  return parseInt(<string>_ms, 10) / 1000;
+}
+
+export function secToMs(_s: number | string): number {
+  return parseInt(<string>_s, 10) * 1000;
+}
+
+export function msToMins(_ms: number | string): number {
+  return msToSec(_ms) / 60;
+}
+
+export function minToMs(_m: number | string): number {
+  return secToMs(_m) * 60;
+}
+
+export function msToHours(_ms: number | string): number {
+  return msToMins(_ms) / 60;
+}
+
+export function hourToMs(_h: number | string): number {
+  return minToMs(_h) * 60;
+}
+
+export function msToDays(_ms: number | string): number {
+  return msToHours(_ms) / 24;
+}
+
+export function dayToMs(_d: number | string): number {
+  return hourToMs(_d) * 24;
+}
