1b0b49015a4d7c55df1f166f82836dce0a8abe88
Jerry Orta <jerryorta.dev@gmail.com>
2022-12-08 20:35:34 -0600

    feat(theme): multi theme

--- apps/patterns/src/app/components/app-navbar/app-navbar.component.ts
+++ apps/patterns/src/app/components/app-navbar/app-navbar.component.ts
@@ -1,11 +1,13 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {Component, Inject} from '@angular/core';
+import {CommonModule, DOCUMENT} from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {
   GithubLogoComponent,
   UiuxLogoComponent
 } from '@uiux/shared/ui-design-library';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'ng-patterns-app-navbar',
@@ -15,7 +17,9 @@ import {
     MatToolbarModule,
     MatIconModule,
     UiuxLogoComponent,
-    GithubLogoComponent
+    GithubLogoComponent,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   templateUrl: './app-navbar.component.html',
@@ -25,4 +29,19 @@ export class AppNavbarComponent {
   logoName = 'uiux-with-angular-logo';
 
   githubLink = 'https://github.com/UIUXEngineering/ng-patterns';
+
+  currentTheme = 'dark-theme';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  selectTheme(theme: string) {
+    if (theme === 'remove') {
+      document.body.classList.remove('light-theme');
+      document.body.classList.remove('dark-theme');
+    } else {
+      document.body.classList.remove(this.currentTheme);
+      this.currentTheme = theme;
+      document.body.classList.add(theme);
+    }
+  }
 }
