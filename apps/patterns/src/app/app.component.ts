7738750db2013a9735b3b1a3a158f90117909272
Jerry Orta <jerryorta.dev@gmail.com>
2022-12-08 20:51:36 -0600

    feat(sidenav): add accordian

--- apps/patterns/src/app/app.component.ts
+++ apps/patterns/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {AppNavbarComponent} from './components/app-navbar/app-navbar.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'ng-patterns-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [AppNavbarComponent, MatSidenavModule]
+  imports: [
+    AppNavbarComponent,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatIconModule,
+    RouterLink
+  ]
 })
 export class AppComponent {
+  // @ViewChild(MatAccordion) accordion: MatAccordion;
   title = 'patterns';
 }
