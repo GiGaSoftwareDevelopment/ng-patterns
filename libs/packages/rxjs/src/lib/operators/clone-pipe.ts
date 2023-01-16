43c457bb44b7d3a9df2fe2d4acf794abbdeb36e4
Jerry Orta <jerryorta.dev@gmail.com>
2023-01-16 02:39:42 -0600

    refactor(rxjs): use cloneDeep for clonePipe

--- libs/packages/rxjs/src/lib/operators/clone-pipe.ts
+++ libs/packages/rxjs/src/lib/operators/clone-pipe.ts
@@ -2,14 +2,14 @@
  * @license
  * Copyright UIUX Engineering All Rights Reserved.
  */
-import { clone } from '@uiux/fn';
+import { cloneDeep } from '@uiux/fn';
 import { OperatorFunction } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export function clonePipe<T>(): OperatorFunction<T, T> {
   return map((d: T) => {
     try {
-      return clone(d);
+      return cloneDeep(d);
     } catch (e: any) {
       return d;
     }
