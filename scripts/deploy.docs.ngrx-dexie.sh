0e54bf1e1f494580db675b2ac5a7883400348217
Jerry Orta <jerryorta.dev@gmail.com>
2023-01-22 12:17:36 -0600

    docs(compodoc): commands

---
+++ scripts/deploy.docs.ngrx-dexie.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Clear Published Directory
+rm -rf apps/firebase/docs/ngrx-dexie;
+
+# Generate docs from source directory
+cd libs/packages/ngrx-dexie
+../../../node_modules/.bin/compodoc -p tsconfig.doc.json -d ../../../apps/firebase/docs/ngrx-dexie --theme material --disablePrivate;
+
+
+cd ../../../apps/firebase;
+firebase use ng-patterns;
+firebase deploy --only hosting:uiux-docs-ngrx-dexie;
+
