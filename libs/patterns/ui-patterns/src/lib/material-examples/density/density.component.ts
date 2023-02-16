062b79ee81ac10d6a3c824f061ebcf5dede4c106
Jerry Orta <jerryorta.dev@gmail.com>
2023-02-16 05:32:02 -0600

    add density samples NP-54

--- libs/patterns/ui-patterns/src/lib/material-examples/density/density.component.ts
+++ libs/patterns/ui-patterns/src/lib/material-examples/density/density.component.ts
@@ -1,11 +1,19 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'pat-density',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule ],
   templateUrl: './density.component.html',
-  styleUrls: ['./density.component.scss']
+  styleUrls: [ './density.component.scss' ],
+  host: {
+    class: 'sample-page-layout'
+  }
 })
-export class DensityComponent {}
+export class DensityComponent {
+}
