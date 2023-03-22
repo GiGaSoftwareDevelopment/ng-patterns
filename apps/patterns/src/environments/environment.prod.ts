991663d60d18d9b9b30777bc57b4b7680089e980
Jerry Orta <jerryorta.dev@gmail.com>
2023-03-22 05:00:19 -0500

    fix environment config

--- apps/patterns/src/environments/environment.prod.ts
+++ apps/patterns/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-import { createDefaultFirebaseConfig } from '@ngpat/firebase';
 import { firebaseConfig } from '@ngpat/patterns/secrets';
 
 export const environment = {
   production: true,
-  firebaseConfig: createDefaultFirebaseConfig(firebaseConfig)
+  firebaseConfig
 };
