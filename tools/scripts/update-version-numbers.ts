ec1d65ee186c528b9fa1ceb1ce744588d9c4dbf8
Jerry Orta <jerryorta.dev@gmail.com>
2023-05-09 05:45:51 -0500

    chore(packages): bump version to 16.0.8

--- tools/scripts/update-version-numbers.ts
+++ tools/scripts/update-version-numbers.ts
@@ -4,18 +4,16 @@ import * as readline from 'readline';
 import * as glob from 'glob';
 import { createBuilder } from './util';
 import { packages } from './publish/config';
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 // get the version from the command
 // e.g. ts-node ./build/update-version-numbers.ts 10.0.0
 // const [ newVersion ] = process.argv.slice(2);
-const argv: any = yargs(hideBin(process.argv)).argv
+const argv: any = yargs(hideBin(process.argv)).argv;
 
 async function getVersion(_version: string | undefined): Promise<string> {
   return new Promise((resolve, reject) => {
-
     if (!_version) {
       // if no version is provided, ask for it
       const rl = readline.createInterface({
@@ -27,18 +25,17 @@ async function getVersion(_version: string | undefined): Promise<string> {
         rl.close();
 
         resolve(_newVersion);
-
       });
     } else {
       resolve(_version);
     }
-
-  })
+  });
 }
 
-async function getUpdatePackageVersion(_updatePackageJsonVersion: undefined | boolean): Promise<boolean> {
+async function getUpdatePackageVersion(
+  _updatePackageJsonVersion: undefined | boolean
+): Promise<boolean> {
   return new Promise((resolve, reject) => {
-
     if (_updatePackageJsonVersion === undefined) {
       // if no version is provided, ask for it
       const rl = readline.createInterface({
@@ -46,23 +43,29 @@ async function getUpdatePackageVersion(_updatePackageJsonVersion: undefined | bo
         output: process.stdout
       });
 
-      rl.question(`Do you want to update versions of packages? y|n `, (_updatePackageVersion: string) => {
-        rl.close();
-        resolve(_updatePackageVersion === 'y');
-      });
+      rl.question(
+        `Do you want to update versions of packages? y|n `,
+        (_updatePackageVersion: string) => {
+          rl.close();
+          resolve(
+            !(
+              _updatePackageVersion === 'n' ||
+              _updatePackageVersion === 'N' ||
+              _updatePackageVersion === 'no'
+            )
+          );
+        }
+      );
     } else {
       resolve(_updatePackageJsonVersion);
     }
-
-
-  })
-
-
+  });
 }
 
-async function getUpdatePeerDependencies(_updatePeerDependencies: undefined | boolean): Promise<boolean> {
+async function getUpdatePeerDependencies(
+  _updatePeerDependencies: undefined | boolean
+): Promise<boolean> {
   return new Promise((resolve, reject) => {
-
     if (_updatePeerDependencies === undefined) {
       // if no version is provided, ask for it
       const rl = readline.createInterface({
@@ -70,32 +73,50 @@ async function getUpdatePeerDependencies(_updatePeerDependencies: undefined | bo
         output: process.stdout
       });
 
-      rl.question(`Do you want to update version in peer dependencies? y|n `, (_updatePackageVersion: string) => {
-        rl.close();
-        resolve(_updatePackageVersion === 'y');
-      });
+      rl.question(
+        `Do you want to update version in peer dependencies? y|n `,
+        (_updatePackageVersion: string) => {
+          rl.close();
+          resolve(
+            !(
+              _updatePackageVersion === 'n' ||
+              _updatePackageVersion === 'N' ||
+              _updatePackageVersion === 'no'
+            )
+          );
+        }
+      );
     } else {
       resolve(_updatePeerDependencies);
     }
-
-
-  })
-
-
+  });
 }
-
 
 async function start() {
   const version = await getVersion(argv?.newVersion);
-  const updatePackageVersion = await getUpdatePackageVersion(argv?.updatePackageVersion);
-  const updatePeerDependencies = await getUpdatePeerDependencies(argv?.updatePeerDependencies);
+  const updatePackageVersion = await getUpdatePackageVersion(
+    argv?.updatePackageVersion
+  );
+  const updatePeerDependencies = await getUpdatePeerDependencies(
+    argv?.updatePeerDependencies
+  );
   updateVersions(version, updatePackageVersion, updatePeerDependencies);
 }
 
-
-function updateVersions(version: string, updatePackageVersion: boolean, updatePeerDependencies: boolean) {
+function updateVersions(
+  version: string,
+  updatePackageVersion: boolean,
+  updatePeerDependencies: boolean
+) {
   const publishNext = createBuilder([
-    [ 'Update package.json', createPackageJsonBuilder(version, updatePackageVersion, updatePeerDependencies) ]
+    [
+      'Update package.json',
+      createPackageJsonBuilder(
+        version,
+        updatePackageVersion,
+        updatePeerDependencies
+      )
+    ]
     // ['Update ng-add schematic', createUpdateAddSchematicBuilder(version)],
     // ['Update docs version picker', createArchivePreviousDocsBuilder(version)],
     // ['Create migration docs', createMigrationDocs(version)],
@@ -119,8 +140,12 @@ function addAtLeastMinorVersion(version: string): string {
  * Updates package versions in package.json files
  * Updates peerDependencies versions of NgRx packages in package.json files
  */
-function createPackageJsonBuilder(version: string, updatePackageVersion: boolean, updatePeerDependencies: boolean) {
-  const [ major ] = version.split('.');
+function createPackageJsonBuilder(
+  version: string,
+  updatePackageVersion: boolean,
+  updatePeerDependencies: boolean
+) {
+  const [major] = version.split('.');
   return async () => {
     glob
       .sync('**/package.json', { ignore: '**/node_modules/**' })
@@ -129,8 +154,13 @@ function createPackageJsonBuilder(version: string, updatePackageVersion: boolean
         const pkg = JSON.parse(content);
         let saveFile = false;
 
+        console.log('\n\n');
+        console.log(pkg?.name);
+
         if (
-          (updatePackageVersion && pkg?.version && pkg?.name?.startsWith('@ngpat')) ||
+          (updatePackageVersion &&
+            pkg?.version &&
+            pkg?.name?.startsWith('@ngpat')) ||
           pkg?.name?.startsWith('nx-ng-mat-prototype')
         ) {
           pkg.version = version;
@@ -145,11 +175,13 @@ function createPackageJsonBuilder(version: string, updatePackageVersion: boolean
             ) {
               pkg.peerDependencies[key] = addAtLeastMinorVersion(version);
               saveFile = true;
+              console.log(key, pkg.peerDependencies[key]);
             } else if (key.startsWith('@angular')) {
               // because the NgRx version is in sync with the Angular version
               // we can also update the Angular dependencies
               pkg.peerDependencies[key] = `^${major}.0.0`;
               saveFile = true;
+              console.log(key, pkg.peerDependencies[key]);
             }
           });
         }
