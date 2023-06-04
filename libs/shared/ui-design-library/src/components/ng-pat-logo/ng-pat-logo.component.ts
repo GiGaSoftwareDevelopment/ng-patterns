5594fb1f3a175d6dc37d79c0749544bdda8cb133
Jerry Orta <jerryorta.dev@gmail.com>
2023-06-04 13:26:54 -0500

    feat(logo): update

--- libs/shared/ui-design-library/src/components/ng-pat-logo/ng-pat-logo.component.ts
+++ libs/shared/ui-design-library/src/components/ng-pat-logo/ng-pat-logo.component.ts
@@ -1,16 +1,20 @@
 import {
   ChangeDetectionStrategy,
-  Component, EventEmitter, HostBinding, Input, Output,
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output,
   ViewEncapsulation
 } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'design-library-ng-pat-logo',
   standalone: true,
-  imports: [ CommonModule, MatIconModule ],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './ng-pat-logo.component.html',
   styleUrls: ['./ng-pat-logo.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -18,21 +22,23 @@ import { DomSanitizer } from '@angular/platform-browser';
   host: {
     class: 'design-library-ng-pat-logo',
     '[style.height.px]': 'this.height',
-    '[style.width.px]': 'this.width',
+    '[style.width.px]': 'this.width'
   }
 })
 export class NgPatLogoComponent {
-
   @HostBinding('class.design-library-ng-pat-logo') hostClass = true;
 
-  ngPatLogo = 'ngPatLogo'
+  ngPatLogo = 'ngPatLogo';
+  ngPatLogoReversed = 'ngPatLogoReversed';
+
+  @Input() reversed = false;
 
   @Output() clickAction: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   private _height = 24;
   @Input()
   set height(value: number) {
-    this._height = value
+    this._height = value;
   }
 
   get height() {
@@ -42,26 +48,26 @@ export class NgPatLogoComponent {
   private _width = 24;
   @Input()
   set width(value: number) {
-    this._width = value
+    this._width = value;
   }
 
   get width() {
     return this._width;
   }
 
-
-  constructor(
-    iconRegistry: MatIconRegistry,
-    sanitizer: DomSanitizer,
-  ) {
-
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
       this.ngPatLogo,
+      sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/ui-design-library/ng-pat-logo.svg'
+      )
+    );
+
+    iconRegistry.addSvgIcon(
+      this.ngPatLogoReversed,
       sanitizer.bypassSecurityTrustResourceUrl(
         'assets/ui-design-library/ng-pat-logo-reversed.svg'
       )
     );
-
   }
-
 }
