eff2934926932f4cfa9607f0f9ce5a2779ee5e41
Jerry Orta <jerryorta.dev@gmail.com>
2023-08-19 05:27:43 -0500

    remove firebaseui and yarn

--- libs/packages/material/firebaseui/src/lib/app-login-page/app-login-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-import { AppLoginPageComponent } from './app-login-page.component';
-
-
-export const APP_LOGIN_ROUTES: Route[] = [
-  {path: '', component: AppLoginPageComponent}
-];
--- libs/packages/material/firebaseui/src/lib/remote-login-page/remote-login-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-import { RemoteLoginComponent } from './remote-login.component';
-
-
-export const REMOTE_LOGIN_ROUTES: Route[] = [
-  {path: '', component: RemoteLoginComponent}
-];
