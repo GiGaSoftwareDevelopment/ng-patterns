a761e4dd5edcf05c03a55a3354c44ca73e0fa016
Jerry Orta <jerryorta.dev@gmail.com>
2023-09-03 06:46:09 -0500

    build 16.7.17

--- libs/packages/store/src/lib/+websocket-registry/ng-pat-abstract-connection.service.ts
+++ libs/packages/store/src/lib/+websocket-registry/ng-pat-abstract-connection.service.ts
@@ -1,55 +1,64 @@
 import { NgPatFirestoreService } from '@ngpat/firebase';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { NgPatAccountState } from '../+account/account.model';
+import { selectNgPatAccountState } from '../+account/account.selectors';
 import { NgPatFirestoreWebSocketConnectorService } from '../services/ng-pat-firestore-web-socket-connector.service';
 import { NgPatFirebaseConnectionService } from './websocket-registry.models';
-import { NgPatAccountState } from '../+account/account.model';
-import { Store } from '@ngrx/store';
-import { selectNgPatAccountState } from '../+account/account.selectors';
-import { take } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 export abstract class NgPatAbstractConnectionService
-  implements NgPatFirebaseConnectionService
-{
-  constructor(
-    protected featureKey: string,
-    protected customFirestoreService: NgPatFirestoreService,
-    protected connector: NgPatFirestoreWebSocketConnectorService,
-    protected store: Store
-  ) {
+    implements NgPatFirebaseConnectionService {
+    private _config: { [key: string]: any } = {};
 
-    if (this.ngPatOnInit) {
-      this.ngPatOnInit.call(this);
+    get config(): { [key: string]: any } {
+        return this._config;
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const that = this;
+    constructor(
+        protected featureKey: string,
+        protected customFirestoreService: NgPatFirestoreService,
+        protected connector: NgPatFirestoreWebSocketConnectorService,
+        protected store: Store,
+        config?: { [key: string]: any }
+    ) {
 
-    this.connector.registerWebsocketKey(this.featureKey);
+        this._config = config || {};
 
-    this.connector.onConnect$.subscribe((user: NgPatAccountState) => {
-      this.onConnect.call(that, user);
-    });
+        if (this.ngPatOnInit) {
+            this.ngPatOnInit.call(this);
+        }
 
-    this.connector.onDisconnect$.subscribe((user: NgPatAccountState) => {
-      this.onDisconnect.call(that, user);
-    });
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        const that = this;
 
-  }
+        this.connector.registerWebsocketKey(this.featureKey);
+
+        this.connector.onConnect$.subscribe((user: NgPatAccountState) => {
+            this.onConnect.call(that, user);
+        });
+
+        this.connector.onDisconnect$.subscribe((user: NgPatAccountState) => {
+            this.onDisconnect.call(that, user);
+        });
+
+    }
 
 
-  abstract ngPatOnInit(): void;
+    abstract ngPatOnInit(): void;
 
-  abstract onConnect(user: NgPatAccountState): void;
-  abstract onDisconnect(user: NgPatAccountState): void;
+    abstract onConnect(user: NgPatAccountState): void;
 
-  selectUser(): Observable<NgPatAccountState> {
-    return this.store.select(selectNgPatAccountState).pipe(take(1));
-  }
+    abstract onDisconnect(user: NgPatAccountState): void;
 
-  destroy() {
-    this.selectUser().subscribe((user: NgPatAccountState) => {
-      this.onDisconnect(user);
-      this.connector.deleteKey(this.featureKey);
-    });
-  }
+    selectUser(): Observable<NgPatAccountState> {
+        return this.store.select(selectNgPatAccountState).pipe(take(1));
+    }
+
+    destroy() {
+        this.selectUser().subscribe((user: NgPatAccountState) => {
+            this.onDisconnect(user);
+            this.connector.deleteKey(this.featureKey);
+        });
+    }
 }
