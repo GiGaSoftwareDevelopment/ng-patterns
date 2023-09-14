c9f8bc56b00174294ac20be832e1d260a76c8042
Jerry Orta <jerryorta.dev@gmail.com>
2023-09-14 05:54:03 -0500

    feat(store): add memoized selector to track entities added and removed from store

--- libs/packages/store/src/lib/selectors.ts
+++ libs/packages/store/src/lib/selectors.ts
@@ -3,8 +3,9 @@ import { Dictionary } from '@ngrx/entity/src/models';
 
 
 /**
+ * Returns entities that have been deleted from the store.
  *
- * @param entitySelector = the selector for enitites in an NgRX Entity Store
+ * @param entitySelector the selector for enitites in an NgRX Entity Store
  */
 export const selectNgPatDeletedEntities = <T>(entitySelector: MemoizedSelector<any, any>) => {
 
@@ -27,6 +28,75 @@ export const selectNgPatDeletedEntities = <T>(entitySelector: MemoizedSelector<a
       };
 
       return Object.values(deletedEntities);
+    }
+  );
+}
+
+export interface NgPatAddedAndDeletedEntities<T> {
+  addedEntities: T[];
+  deletedEntities: T[];
+}
+
+/**
+ *
+ * Returns entites that have been added and deleted from the store.
+ *
+ * @param entitySelector the selector for enitites in an NgRX Entity Store
+ */
+export const selectNgPatAddedAndDeletedEntities = <T>(entitySelector: MemoizedSelector<any, any>) => {
+
+  let currentEntities: Dictionary<T> = {}
+
+  return createSelector(
+    entitySelector,
+    (newEntities: Dictionary<T>): NgPatAddedAndDeletedEntities<T> => {
+
+      /**
+       * Look through current entities and find any that are not in the new entities
+       */
+      const deletedEntities = Object.keys(currentEntities)
+        .reduce((result: { [key: string]: T }, key: string) => {
+          /**
+           * If the current entity is not in the new entities, then it has been deleted
+           */
+          if (!newEntities[key] && currentEntities[key]) {
+            /**
+             * Add the deleted entity to the result
+             */
+            result[key] = <T>currentEntities[key];
+          }
+          return result;
+        }, {});
+
+      /**
+       * Look through new entities and find any that are not in the current entities
+       */
+      const addedEntities = Object.keys(newEntities)
+        .reduce((result: { [key: string]: T }, key: string) => {
+          /**
+           * If the new entity is not in the current entities, then it has been added
+           */
+          if (!currentEntities[key] && newEntities[key]) {
+            /**
+             * Add the added entity to the result
+             */
+            result[key] = <T>newEntities[key];
+          }
+          return result;
+        }, {});
+
+      /**
+       * Update the current entities
+       */
+      currentEntities = {
+        ...newEntities
+      };
+
+      return {
+        addedEntities: Object.values(addedEntities),
+        deletedEntities: Object.values(deletedEntities)
+      }
+
     }
   );
 }
