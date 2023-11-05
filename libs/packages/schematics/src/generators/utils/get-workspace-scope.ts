a6f179e46e535d91a04faf1401fb82d6200aa3d0
Jerry Orta <jerryorta.dev@gmail.com>
2023-11-05 16:18:28 -0600

    build(packages): 16.9.1

--- libs/packages/schematics/src/generators/utils/get-workspace-scope.ts
+++ libs/packages/schematics/src/generators/utils/get-workspace-scope.ts
@@ -2,6 +2,6 @@ import { Tree, readNxJson } from '@nx/devkit';
 
 export function getWorkspaceScope(tree: Tree) {
   const wsConfig = readNxJson(tree);
-  const workspaceName = `@${wsConfig?.npmScope}`;
+  const workspaceName = `@${(<any>wsConfig)?.npmScope}`;
   return workspaceName;
 }
