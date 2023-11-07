90b6bc4932bc01b7f34cc330bb5864bd6c4dea26
Jerry Orta <jerryorta.dev@gmail.com>
2023-11-06 18:29:39 -0600

    feat(utils): process-queue
    move constructor params to getter and setters

--- libs/packages/utils/src/lib/process-queue.ts
+++ libs/packages/utils/src/lib/process-queue.ts
@@ -10,8 +10,20 @@ export interface NgPatQueueItem<T> {
   config: T;
 }
 
+/**
+ * A function that compares two items and returns a boolean
+ * to determine if an id or item is contained the queue.
+ */
 export type NgPatProcessQueueFindFn = (a: any, b: any, id?: string) => boolean;
 
+/**
+ * A function that compares two items and returns a boolean
+ * to determine if an id or item is contained the queue.
+ *
+ * This is the default find function that assumes the queue
+ * is a collection of objects. You can optionally pass in
+ * an id to compare the objects by a specific property.
+ */
 export function ngPatProcessQueueFindFnByKey<T>(
   a: any,
   b: any,
@@ -24,20 +36,41 @@ export function ngPatProcessQueueFindFnByKey<T>(
   return a === b;
 }
 
+/**
+ * An Async Iterator that processes a queue of items.
+ */
 export class NgPatProcessQueue<T> {
   private _queue: T[] = [];
   private _queue$: ReplaySubject<T> = new ReplaySubject<T>(1);
 
   currentItem$: Observable<T> = this._queue$.asObservable();
 
+  private _findFn: NgPatProcessQueueFindFn = ngPatProcessQueueFindFnByKey;
+
+  get findFn(): NgPatProcessQueueFindFn {
+    return this._findFn;
+  }
+
+  set findFn(value: NgPatProcessQueueFindFn) {
+    this._findFn = value;
+  }
+
+  private _key: string | undefined;
+
+  get key(): string | undefined {
+    return this._key;
+  }
+
+  set key(value: string | undefined) {
+    this._key = value;
+  }
+
   constructor(
-    private findFn: NgPatProcessQueueFindFn = ngPatProcessQueueFindFnByKey,
-    private id?: string
   ) {
     // TODO Do we need this?
-    this._queue$.subscribe(() => {
-      this.next();
-    });
+    // this._queue$.subscribe(() => {
+    //   this.next();
+    // });
   }
 
   addItem(item: T | T[]) {
@@ -56,7 +89,7 @@ export class NgPatProcessQueue<T> {
 
   addUnique(item: any) {
     const found = this._queue.find((a: any) => {
-      return this.findFn(item, a, this.id);
+      return this.findFn(item, a, this.key);
     });
 
     if (!found) {
